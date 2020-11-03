52d31b73d3624c7cbb5cb8f2110dbbc3e5005869
bbastardes <blancabastardes@gmail.com>
2020-11-03 12:59:58 +0100

    Jupyter notebook password set--amend

--- run.sh
+++ run.sh
@@ -10,6 +10,7 @@ DATA_DIR=$ROOT_DIR/data
 CODE_DIR=$ROOT_DIR/src
 
 nvidia-docker  run   \
+	-e JUPYTER_TOKEN="acic_bughunters" \
 	-v $DATA_DIR:/data \
 	-v $CODE_DIR:/src \
 	-it custom_docker_image \
