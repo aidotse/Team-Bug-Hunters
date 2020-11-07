669eeac53e31b678e0c05da81f4eabe6fe4f336e
bbastardes <[blancabastardes@gmail.com]>
2020-11-07 20:00:25 +0100

    Parameters

---
+++ src/params.py
@@ -0,0 +1,13 @@
+flip_prob = 0.5
+rot_angle = 45
+rot_prob = 0.5
+brightness_limit = 0.05
+contrast_limit = 0.05
+bright_cont_prob = 0.5
+alpha = 0.5
+sigma = 2
+points = 4
+elastic_prob = 1
+prob_all = 1
+magnification = 20
+fluor
